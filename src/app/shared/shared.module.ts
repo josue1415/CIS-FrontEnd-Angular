aed2de4afe21753d273448eb5cce4f4f33665a3c
josue1415 <josue1415@yahoo.es>
2022-10-22 17:08:28 -0600

    arreglo de error

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,9 +6,6 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { StriphtmlPipe } from './pipes/striphtml.pipe';
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-
-
 
 // Export using to translate
 export function HttpLoaderFactory(http: HttpClient) {
